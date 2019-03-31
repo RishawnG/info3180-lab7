6fb8eb8131fd05c0b0d589bc450dadf7ed0203ca
Rishawn Gregory <rishawn.gregory@mymona.uwi.edu>
2019-03-31 16:50:25 +0000

    ex 1

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,8 @@
+from flask_wtf import Form,TextAreaField,DataRequired
+from flask_wtf.file import FileField,FileRequired,FileAllowed
+from werkzeug import secure_filename
 
+class UploadForm(Form):
+    description=TextAreaField('description',validators=[DataRequired()])
+    fileupload=FileField(validators=[FileRequired(),FileAllowed(['jpg', 'png', 'Images only!'])])
+    
